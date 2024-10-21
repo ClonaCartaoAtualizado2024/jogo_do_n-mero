1434854909935d99e1d8619b829307dd8890ea07
ClonaCartaoAtualizado2024 <163008911+ClonaCartaoAtualizado2024@users.noreply.github.com>
2024-10-21 11:32:40 -0300

    .

--- main.js
+++ main.js
@@ -6,3 +6,31 @@
     const campoPalpite = document.querySelector('.campoPalpite');
     let contagemPalpites = 1;
     let botaoReiniciar;
+
+function verificarPalpite() {
+   const palpiteUsuario = Number (campoPalpite.value);
+   if (contagemPalpites === 1) {
+      palpites.textContent = "palpites anteriores: ";
+   }
+
+palpites.textContent += palpiteUsuario + " ";
+
+   if (palpiteUsuario === numeroAleatorio) {
+      ultimoResultado.textContent = "boa man, fez o minimo"
+      ultimoResultado.style.backgroundColor = "green";
+      baixoOuAlto.textContent = "";
+      finalizarJogo();
+   } else if (contagemPalpites === 10) {
+      ultimoResultado.textContent = "cabo manin";
+      baixoOuAlto.textContent = "";
+      finalizarJogo();
+   } else {
+      ultimoResultado.textContent = "errado"
+      ultimoResultado.style.backgroundColor = "red";
+      if (palpiteUsuario < numeroAleatorio) {
+         baixoOuAlto.textContent = "Esse palpite ta muito alanna"
+      } else if (palpiteUsuario > numeroAleatorio) {
+         baixoOuAlto.textContent = "esse palpite ta muito pierre";
+      } 
+   }
+}
